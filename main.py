27b7227fab14515236b765bb73c4da776a158a58
mezoaziz12340-jpg <mezoaziz12340@gmail.com>
2026-05-22 23:17:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-   uvicorn main:app --host 0.0.0.0 --port $PORT
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from Batana Lectures!"}
